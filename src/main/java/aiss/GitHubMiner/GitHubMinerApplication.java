04e09ba5bc31aac2f8efa7e948ad757e1b012d36
sofia3015 <sofialuna3015@gmail.com>
2025-05-05 18:30:03 +0200

    Primer commit - Proyecto desde cero

---
+++ src/main/java/aiss/GitHubMiner/GitHubMinerApplication.java
@@ -0,0 +1,13 @@
+package aiss.GitHubMiner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubMinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubMinerApplication.class, args);
+	}
+
+}
