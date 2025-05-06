cb617ae1e14864cfc05d6852261b33955fbce7cf
sofia3015 <147925605+sofia3015@users.noreply.github.com>
2025-05-06 10:27:51 +0200

    Update GitHubService.java

--- src/main/java/aiss/GitHubMiner/service/GitHubService.java
+++ src/main/java/aiss/GitHubMiner/service/GitHubService.java
@@ -12,6 +12,13 @@ import java.time.format.DateTimeFormatter;
 
 @Service
 public class GitHubService {
+
+     @Value("La uri de mi repositorio")
+    private String baseUri;
+
+    @Value("Mi token")
+    private String token;
+    
     @Autowired
     RestTemplate restTemplate;
     public Project getProject(String owner, String repo){
@@ -27,5 +34,38 @@ public class GitHubService {
         String url = "https://api.github.com/repos/" + owner + "/" + repo + "/issues" + "?since=" + day + "&page=" + page;
         Issue[] issues = restTemplate.getForObject(url, Issue[].class);
         return issues;
+    }
+
+    public Commit[] getCommits(String owner, String repo) {
+        String url = baseUri + owner + "/" + repo;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + token);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<Commit[]> response = restTemplate.exchange(url, HttpMethod.GET, entity, Commit[].class);
+        return response.getBody();
+    }
+
+    public Comment[] getComment(String owner, String repo) {
+        String url = baseUri + owner + "/" + repo;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + token);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<Comment[]> response = restTemplate.exchange(url, HttpMethod.GET, entity, Comment[].class);
+        return response.getBody();
+    }
+
+    public User getUser(String owner, String repo) {
+        String url = baseUri + owner + "/" + repo;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + token);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity <User> response = restTemplate.exchange(url, HttpMethod.GET, entity, User.class);
+        return response.getBody();
     }
 }
