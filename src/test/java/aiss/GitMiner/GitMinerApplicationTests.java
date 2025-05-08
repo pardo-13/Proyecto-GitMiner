4054d3d65cdfe6a2c591c5c76f52079ecbb09a4e
DARIO SEGURA <darioysegura1@gmail.com>
2025-05-08 22:02:01 +0200

    Initial Commit

---
+++ src/test/java/aiss/GitMiner/GitMinerApplicationTests.java
@@ -0,0 +1,13 @@
+package aiss.GitMiner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitMinerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
