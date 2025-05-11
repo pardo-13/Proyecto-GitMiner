a3250bb9d24208fb401b3ab56c17fc630a5fcdea
DARIO SEGURA <darioysegura1@gmail.com>
2025-05-11 17:03:53 +0200

    Arreglo mapping

--- src/main/java/aiss/GitMiner/Controllers/GitMinerControllers.java
+++ src/main/java/aiss/GitMiner/Controllers/GitMinerControllers.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("gitminer/projects")
+@RequestMapping("gitminer/")
 public class GitMinerControllers {
 
     @Autowired
